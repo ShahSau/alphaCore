1bce25fedd6a6912df0e269ab57788ffda43f68b
shahriar <shahriarksaurov@gmail.com>
2023-07-23 01:38:28 +0300

    sql

---
+++ app/api/sql/route.ts
@@ -0,0 +1,80 @@
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+const configuration  = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const instructionMessage: ChatCompletionRequestMessage = {
+    role: "system",
+    content: `Given the following SQL tables, your job is to write queries given a user’s request.
+    
+    CREATE TABLE Orders (
+      OrderID int,
+      CustomerID int,
+      OrderDate datetime,
+      OrderTime varchar(8),
+      PRIMARY KEY (OrderID)
+    );
+    
+    CREATE TABLE OrderDetails (
+      OrderDetailID int,
+      OrderID int,
+      ProductID int,
+      Quantity int,
+      PRIMARY KEY (OrderDetailID)
+    );
+    
+    CREATE TABLE Products (
+      ProductID int,
+      ProductName varchar(50),
+      Category varchar(50),
+      UnitPrice decimal(10, 2),
+      Stock int,
+      PRIMARY KEY (ProductID)
+    );
+    
+    CREATE TABLE Customers (
+      CustomerID int,
+      FirstName varchar(50),
+      LastName varchar(50),
+      Email varchar(100),
+      Phone varchar(20),
+      PRIMARY KEY (CustomerID)
+    );
+    `
+  };
+
+export async function POST(
+    req: Request
+){
+   try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages  } = body;
+    
+    if (!userId) {
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!configuration.apiKey) {
+        return new NextResponse("OpenAI API Key not configured.", { status: 500 });
+    }
+
+    if (!messages) {
+        return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [instructionMessage, ...messages]
+      });
+
+    return NextResponse.json(response.data.choices[0].message);
+   } catch (error) {
+    console.log('[SQL_ERROR]', error);
+    return new NextResponse("Internal Error", { status: 500 });
+   } 
+}
