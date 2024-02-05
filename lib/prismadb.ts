667e37831416d4fecf4bbd4f7cac90ec17dee1ee
shahriar <shahriarksaurov@gmail.com>
2024-02-05 21:31:07 +0100

    update

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -5,7 +5,6 @@ declare global {
 }
 
 const prismadb = globalThis.prisma || new PrismaClient()
-// const prismadb = typeof window !== "undefined" ? {} as unknown as PrismaClient : new PrismaClient()
 // const prismadb = globalThis.prisma 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
 
